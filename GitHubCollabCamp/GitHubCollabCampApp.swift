b1496df56f77b49193c7cfbe47af16bad79ae3ea
Avanti Singh <avantisingh19@gmail.com>
2025-06-09 09:38:09 -0500

    Initial Commit

---
+++ GitHubCollabCamp/GitHubCollabCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCollabCampApp.swift
+//  GitHubCollabCamp
+//
+//  Created by Scholar on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCollabCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
